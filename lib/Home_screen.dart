cda1413fd60efe7aa717effa4cab9c5dbb41d275
Marwan Atef <atefmarwan60@gmail.com>
2024-12-21 09:02:56 +0200

    Added splash screen

--- lib/Home_screen.dart
+++ lib/Home_screen.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
-  const HomeScreen({super.key});
-
   @override
   State<HomeScreen> createState() => _HomeScreenState();
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:islami/Home_screen.dart';
-
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 void main(){
   runApp(IslamiApp());
 
